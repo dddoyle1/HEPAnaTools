b808ffbb71749ea8713640c3e2ab44e561a4b9c7
Derek Doyle <derek.doyle@colostate.edu>
2021-08-12 15:48:58 -0600

    Program to trim and inspect event-matched tables

---
+++ python/HEPAnaTools/EventCDFShift/trim_tables.py
@@ -0,0 +1,29 @@
+from HEPAnaTools.EventCDFShift import EventMatchedTable
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input')
+parser.add_argument('output')
+parser.add_argument('--keep_vars', nargs='+', default=None)
+parser.add_argument('--keep_cuts', nargs='+', default=None)
+parser.add_argument('--keep_samples', nargs='+', default=None)
+parser.add_argument('--inspect', nargs='*')
+
+args = parser.parse_args()
+
+if type(args.inspect) is list:
+    EventMatchedTable.Inspect(args.input,
+                              only=args.inspect)
+    
+else:
+    tables = EventMatchedTable.FromH5(args.input)
+    if not args.keep_vars: args.keep_vars = tables.var_labels
+    if not args.keep_cuts: args.keep_cuts = tables.cut_labels
+    if not args.keep_samples: args.keep_samples = tables.samples
+    
+    new_tables = tables.Trim(keep_vars=args.keep_vars,
+                             keep_cuts=args.keep_cuts,
+                             keep_samples=args.keep_samples)
+
+    new_tables.ToH5(args.output)
+
