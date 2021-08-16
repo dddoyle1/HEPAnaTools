42444a1b31e55e0bd14818a0bc1c92965c730522
Derek Doyle <derek.doyle@colostate.edu>
2021-08-16 11:39:25 -0600

    Add Hist3D

--- python/hepanatools/shift/hist.py
+++ python/hepanatools/shift/hist.py
@@ -25,18 +25,20 @@ class FileNameOrHandle:
 
         
 class Axis:
-    def __init__(self, bins):
-        self.bins = bins
-        
-    def find_bin(self, x, **kwargs):
-        return np.digitize(x, self.bins, **kwargs)[0]
+    def __init__(self, edges):
+        self.edges = edges
+    def NBins(self):
+        return len(self.edges) - 1
+    
+    def FindBin(self, x, **kwargs):
+        return np.digitize(x, self.edges, **kwargs) - 1
 
-    def get_bin_center(self, i):
-        return (self.bins[i+1] + self.bins[i]) / 2
+    def GetBinCenter(self, i):
+        return (self.edges[i+1] + self.edges[i]) / 2
 
 
-class Hist:
-    def __init__(self, X, bins, **kwargs):
+class Hist1D:
+    def __init__(self, X, edges, **kwargs):
         self.n, edges = np.histogram(X, bins, **kwargs)
         self.xaxis = Axis(edges)
 
@@ -44,38 +46,41 @@ class Hist:
     def FromH5(file_name_or_handle, path):
         with FileNameOrHandle(file_name_or_handle, 'r') as f:
             dset = f.get(path)[:]
-            h = Hist.FilledHist(dset[:], dset.attrs['xbins'])
+            h = Hist.FilledHist(dset[:], dset.attrs['xedges'])
         return h
 
     @staticmethod
-    def Filled(n, bins):
-        h = Hist([], [])
+    def Filled(n, edges):
+        h = Hist1D([], [])
         h.n = n
-        h.xaxis = bins if type(bins) is Axis else Axis(bins)
+        h.xaxis = edges if type(edges) is Axis else Axis(edges)
         return h
         
 
+    def FindBin(self, x):
+        return self.xaxis.FindBin(x)
+    
     def ToH5(self, file_name_or_handle, path):
         with FileNameOrHandle(file_name_or_handle, 'w') as f:
             dset = f.create_dataset(path, data=self.n, compression='gzip')
-            dset.attrs['xaxis'] = self.xaxis.bins
+            dset.attrs['xaxis'] = self.xaxis.edges
 
     def ToROOT(self, output, name):
         import ROOT
         from array import array
-        h = ROOT.TH1D('', '', len(self.xaxis.bins), array('d', self.xaxis.bins))
-        for i in range(len(self.xaxis.bins)):
+        h = ROOT.TH1D('', '', len(self.xaxis.edges), array('d', self.xaxis.edges))
+        for i in range(len(self.xaxis.edges)):
             h.SetBinContent(i+1, self.n[i])
         h.Write(name)
 
 
     def Draw(self, ax, **kwargs):
-        ax.hist(self.xaxis.bins[:-1], bins=self.xaxis.bins, weights=self.n, **kwargs)
-        ax.set_xlim([self.xaxis.bins[0], self.xaxis.bins[-1]])        
+        ax.hist(self.xaxis.edges[:-1], edges=self.xaxis.edges, weights=self.n, **kwargs)
+        ax.set_xlim([self.xaxis.edges[0], self.xaxis.edges[-1]])        
 
 class Hist2D:
-    def __init__(self, X, Y, bins, **kwargs):
-        self.n, xedges, yedges = np.histogram(X, Y, bins, **kwargs)
+    def __init__(self, X, Y, edges, **kwargs):
+        self.n, xedges, yedges = np.histogram2d(X, Y, bins=edges, **kwargs)
         self.xaxis = Axis(xedges)
         self.yaxis = Axis(yedges)
 
@@ -83,41 +88,119 @@ class Hist2D:
     def FromH5(file_name_or_handle, path):
         with FileNameOrHandle(file_name_or_handle, 'r') as f:
             dset = f.get(path)[:]
-            h = Hist2D.FilledHist(dset[:], dset.attrs['xbins'], dset.attrs['ybins'])
+            h = Hist2D.FilledHist(dset[:], dset.attrs['xedges'], dset.attrs['yedges'])
         return h
 
     @staticmethod
-    def Filled(n, xbins, ybins):
-        h = Hist([], [], [])
+    def Filled(n, xedges, yedges):
+        h = Hist2D([], [], [])
         h.n = n
-        h.xaxis = xbins if type(xbins) is Axis else Axis(xbins)
-        h.yaxis = ybins if type(ybins) is Axis else Axis(ybins)
+        h.xaxis = xedges if type(xedges) is Axis else Axis(xedges)
+        h.yaxis = yedges if type(yedges) is Axis else Axis(yedges)
         return h
-        
+
+    def FindBin(self, x, y, gid=False):
+        ix = self.xaxis.FindBin(x)
+        iy = self.yaxis.FindBin(y)
+        if not gid:
+            return ix, iy
+        else:
+            return ix * self.n.shape[1] + iy 
 
     def ToH5(self, file_name_or_handle, path):
         with FileNameOrHandle(file_name_or_handle, 'w') as f:
             dset = f.create_dataset(path, data=self.n, compression='gzip')
-            dset.attrs['xaxis'] = self.xaxis.bins
-            dset.attrs['yaxis'] = self.yaxis.bins
+            dset.attrs['xaxis'] = self.xaxis.edges
+            dset.attrs['yaxis'] = self.yaxis.edges
             
     def ToROOT(self, output, name):
         import ROOT
         from array import array
         h = ROOT.TH2D('', '',
-                      len(self.xaxis.bins), array('d', self.xaxis.bins),
-                      len(self.yaxis.bins), array('d', self.yaxis.bins))
+                      len(self.xaxis.edges), array('d', self.xaxis.edges),
+                      len(self.yaxis.edges), array('d', self.yaxis.edges))
         
-        for i in range(len(self.xaxis.bins)):
+        for i in range(len(self.xaxis.edges)):
             h.SetBinContent(i+1, self.n[i])
         h.Write(name)
-        
+
+    def ToHist1D(self):
+        return Hist1D.Filled(self.n.flatten(),
+                             np.linspace(0,
+                                         np.prod(*self.n.shape),
+                                         np.prod(*self.n.shape)+1))        
+    
     def Draw(self, ax, colorbar=True,**kwargs):
-        _X, _Y = np.meshgrid(self.xaxis.bins, self.yaxis)
+        _X, _Y = np.meshgrid(self.xaxis.edges, self.yaxis)
         im = ax.pcolormash(_X, _Y, self.n.T, **kwargs)
-        ax.set_xlim([self.xaxis.bins[0], self.xaxis.bins[-1]])
-        ax.set_ylim([self.yaxis.bins[0], self.yaxis.bins[-1]])        
+        ax.set_xlim([self.xaxis.edges[0], self.xaxis.edges[-1]])
+        ax.set_ylim([self.yaxis.edges[0], self.yaxis.edges[-1]])        
         if colorbar: plt.colorbar(im)
+
+                                  
+class Hist3D:
+    def __init__(self, X, Y, Z, edges, **kwargs):
+        self.n, edges = np.histogramdd((X, Y, Z),
+                                       bins=edges,
+                                       **kwargs)
+        self.xaxis = Axis(edges[0])
+        self.yaxis = Axis(edges[1])
+        self.zaxis = Axis(edges[2])
+
+    @staticmethod
+    def FromH5(file_name_or_handle, path):
+        with FileNameOrHandle(file_name_or_handle, 'r') as f:
+            dset = f.get(path)[:]
+            h = Hist3D.FilledHist(dset[:],
+                                  dset.attrs['xedges'],
+                                  dset.attrs['yedges'],
+                                  dset.attrs['zedges'])
+        return h
+
+    @staticmethod
+    def Filled(n, xedges, yedges, zedges):
+        h = Hist3D([], [], [])
+        h.n = n
+        h.xaxis = xedges if type(xedges) is Axis else Axis(xedges)
+        h.yaxis = yedges if type(yedges) is Axis else Axis(yedges)
+        h.zaxis = zedges if type(yedges) is Axis else Axis(yedges)
+        return h
+
+    def FindBin(self, x, y, z, gid=False):
+        ix = self.xaxis.FindBin(x)
+        iy = self.yaxis.FindBin(y)
+        iz = self.zaxis.FindBin(z)
+        if not gid:
+            return ix, iy, iz
+        else:
+            return (ix * self.n.shape[1] * self.n.shape[2]) + iy * self.n.shape[2] + iz
+
+    def ToH5(self, file_name_or_handle, path):
+        with FileNameOrHandle(file_name_or_handle, 'w') as f:
+            dset = f.create_dataset(path, data=self.n, compression='gzip')
+            dset.attrs['xaxis'] = self.xaxis.edges
+            dset.attrs['yaxis'] = self.yaxis.edges
+            dset.attrs['zaxis'] = self.zaxis.edges
+            
+    def ToROOT(self, output, name):
+        import ROOT
+        from array import array
+        h = ROOT.TH2D('', '',
+                      len(self.xaxis.edges), array('d', self.xaxis.edges),
+                      len(self.yaxis.edges), array('d', self.yaxis.edges))
+        
+        for i in range(len(self.xaxis.edges)):
+            h.SetBinContent(i+1, self.n[i])
+        h.Write(name)
+
+    def ToHist1D(self):
+        return Hist1D.Filled(self.n.flatten(),
+                             np.linspace(0,
+                                         np.prod(*self.n.shape),
+                                         np.prod(*self.n.shape)+1))
+    
+    def Draw(self, ax, colorbar=True,**kwargs):
+        raise NotImplementedError
 
         
     
