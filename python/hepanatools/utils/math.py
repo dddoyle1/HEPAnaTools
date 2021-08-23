fe75a15425dd41f01e4d47e405bde8c68f60354d
Derek Doyle <derek.doyle@colostate.edu>
2021-08-23 13:04:12 -0600

    Covariance and correlation matrix functions

--- python/hepanatools/utils/math.py
+++ python/hepanatools/utils/math.py
@@ -10,3 +10,27 @@ def chisq(x, u):
 @numba.jit(nopython=True)
 def not_quite_chisq(x, u):
     return (((x - u) / u) ** 2).sum()
+
+
+@numba.jit(nopython=True)
+def covariance_matrix(nominal, shift):
+    return [
+        [
+            (shift[i] - nominal[i]) * (shift[j] - nominal[j])
+            for i in range(nominal.shape[0])
+        ]
+        for j in range(nominal.shape[0])
+    ]
+
+
+@numba.jit(nopython=True)
+def correlation_matrix(nominal, shift):
+    return [
+        [
+            (shift[i] - nominal[i])
+            * (shift[j] - nominal[j])
+            / np.sqrt(abs(nominal[i] - shift[i]) * abs(nominal[j] - shift[j]))
+            for i in range(nominal.shape[0])
+        ]
+        for j in range(nominal.shape[0])
+    ]
